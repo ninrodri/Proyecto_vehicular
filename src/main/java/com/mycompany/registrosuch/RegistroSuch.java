96c65566cfd256fe077a15f42e7ff75a3c418c60
nincy <nincy@Nincy>
2025-05-19 22:03:59 -0600

    carga inicial

---
+++ src/main/java/com/mycompany/registrosuch/RegistroSuch.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrosuch;
+
+/**
+ *
+ * @author nincy
+ */
+public class RegistroSuch {
+
+    public static void main(String[] args) {
+        Inicio in= new Inicio();
+        in.setVisible(true);
+    }
+}
